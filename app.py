52297fad9b93c7b611d1744388151189f94e83bb
Moises4343 <193243@ids.upchiapas.edu.mx>
2024-07-28 16:34:45 -0600

    optimizacion de recursos

--- app.py
+++ app.py
@@ -151,53 +151,29 @@ gpt2_tokenizer = GPT2Tokenizer.from_pretrained(gpt2_model_name)
 
 # Función para corregir ortografía usando GPT-2
 def corregir_ortografia(texto):
-    # Preprocesar el texto para reemplazar números por letras equivalentes
     texto = texto.replace("4", "a").replace("3", "e").replace("1", "i").replace("0", "o")
-    
-    # Tokenizar el texto de entrada
     inputs = gpt2_tokenizer.encode(texto, return_tensors="pt")
-    attention_mask = torch.ones(inputs.shape, dtype=torch.long)  # Crear una máscara de atención
-
-    # Generar texto completado
+    attention_mask = torch.ones(inputs.shape, dtype=torch.long)
     outputs = gpt2_model.generate(
         inputs, 
-        attention_mask=attention_mask,  # Pasar la máscara de atención
+        attention_mask=attention_mask, 
         max_length=len(inputs[0]) + 1, 
         num_return_sequences=1,
-        pad_token_id=gpt2_tokenizer.eos_token_id  # Establecer pad_token_id al id del token eos
+        pad_token_id=gpt2_tokenizer.eos_token_id
     )
-    
-    # Decodificar la salida generada
     corregido = gpt2_tokenizer.decode(outputs[0], skip_special_tokens=True)
-    
-    # Cortar la salida generada para que coincida con la longitud de entrada
     corregido = corregido[:len(texto)]
-    
     return corregido
 
 class SentimentAnalysis(Resource):
     def preprocess_text(self, text):
-        # Corregir la ortografía antes de cualquier otro procesamiento
         text = corregir_ortografia(text)
-
-        # Convertir a minúsculas
         text = text.lower()
-        
-        # Eliminar puntuaciones, pero mantener signos de exclamación e interrogación
         text = re.sub(r'[^\w\s]', '', text)
-        
-        # Eliminar números
         text = re.sub(r'\d+', '', text)
-        
-        # Remover tildes de forma cuidadosa
         text = text.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u').replace('ñ', 'n')
-        
-        # Eliminar stopwords
         text = ' '.join([word for word in text.split() if word not in stop_words])
-        
-        # Eliminar espacios vacíos
         text = text.strip()
-        
         return text
 
     def detect_groserias(self, text):
@@ -211,21 +187,16 @@ class SentimentAnalysis(Resource):
         data = request.get_json()
         text = data.get('text', '')
 
-        # Validar que el texto no esté vacío
         if not text:
             return jsonify({'error': 'No se proporcionó texto para el análisis.'}), 400
         
         try:
-            # Preprocesamiento del texto
             cleaned_text = self.preprocess_text(text)
 
             if self.detect_groserias(cleaned_text):
                 return jsonify({'error': 'El texto contiene groserías y no puede ser procesado.'}), 400
 
-            # Traducción del texto al inglés
             translated_text = translator.translate(cleaned_text, src='es', dest='en').text
-            
-            # Análisis de sentimiento
             blob = TextBlob(translated_text)
             sentiment = blob.sentiment
             
@@ -243,12 +214,9 @@ class SentimentAnalysis(Resource):
 
 api.add_resource(SentimentAnalysis, '/sentiment')
 
-# Ruta para la URL raíz
 @app.route('/')
 def home():
     return 'Bienvenido a la API de Análisis de Sentimientos'
-
-
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
